cf7c580b0e3b706f47d1a242e2e532d6e75ca6ca
Sona Shrestha <sona.shrestha004@gmail.com>
2021-06-04 14:27:29 +1000

    adding association between tables

--- models/index.js
+++ models/index.js
@@ -1,3 +1,24 @@
 const User = require('./User');
-const Project = require('./Blog');
+const Blog = require('./Blog');
 const Comment = require('./Comment');
+
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+  Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE'
+  }); 
+  
+  Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id'
+  });
+  
+
+  module.exports = { User, Blog , Comment};  
