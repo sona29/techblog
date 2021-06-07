ab9118cc653299c73f5901b6efa3874b3dc94429
Sona Shrestha <sona.shrestha004@gmail.com>
2021-06-07 14:03:33 +1000

    adding association for comment

--- models/index.js
+++ models/index.js
@@ -3,22 +3,30 @@ const Blog = require('./Blog');
 const Comment = require('./Comment');
 
 User.hasMany(Blog, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-  });
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
 
 Blog.belongsTo(User, {
-    foreignKey: 'user_id'
-  });
+  foreignKey: 'user_id',
+});
 
-  Blog.hasMany(Comment, {
-    foreignKey: 'blog_id',
-    onDelete: 'CASCADE'
-  }); 
-  
-  Comment.belongsTo(Blog, {
-    foreignKey: 'blog_id'
-  });
-  
+Blog.hasMany(Comment, {
+  foreignKey: 'blog_id',
+  onDelete: 'CASCADE',
+});
 
-  module.exports = { User, Blog , Comment};  
+Comment.belongsTo(Blog, {
+  foreignKey: 'blog_id',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Blog, Comment };
