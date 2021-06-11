9dc0531d4a6150283c59fbb3786cb7decc01fc86
Sona Shrestha <sona.shrestha004@gmail.com>
2021-06-11 13:51:53 +1000

    adding routes to update blog

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -4,12 +4,11 @@ const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    console.log(req.body);
     const newBlog = await Blog.create({
       ...req.body,
       user_id: req.session.user_id,
     });
-    console.log(newBlog);
+
     res.status(200).json(newBlog);
   } catch (err) {
     res.status(400).json(err);
